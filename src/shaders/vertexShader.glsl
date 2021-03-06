b7df2ff94281638227ef8d973bfc8d20580a324a
Sven <sven.westerlaken@hotmail.nl>
2021-03-06 17:11:09 +0100

    some notes

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -1,7 +1,10 @@
+// v prefix is convention for varying variables, 
+// it is used to pass variables between the vertex & fragment shader.
+// In this case the position of the vertex
 varying vec3 vPosition;
 
 void main() {
-  vPosition = position;
+  vPosition = position; // position is a variable automatically set by ThreeJS
   vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
   gl_Position = projectionMatrix * mvPosition;
   gl_PointSize = 8.0 / -mvPosition.z;
