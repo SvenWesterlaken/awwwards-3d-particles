227afdfac369a232395a17615d1f28bb610b19b2
Sven <sven.westerlaken@hotmail.nl>
2021-03-06 17:03:16 +0100

    add gradient based on position

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -1,4 +1,13 @@
+varying vec3 vPosition;
+
 void main() {
   vec3 color = vec3(1.0, 0.0, 0.0);
+
+  vec3 color1 = vec3(1.0, 0.0, 0.0);
+  vec3 color2 = vec3(1.0, 1.0, 0.0);
+
+
+
+  color = mix(color1, color2, vPosition.z);
   gl_FragColor = vec4(color, 1.0);
 }
