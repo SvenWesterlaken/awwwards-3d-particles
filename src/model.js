e08a7a5b7ad964d3bd2f76103f127f72510a0a70
Sven <sven.westerlaken@hotmail.nl>
2021-03-06 16:17:51 +0100

    create particles geometry

--- src/model.js
+++ src/model.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader'
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler'
 
 class Model {
   constructor (obj) {
@@ -43,13 +44,33 @@ class Model {
       ---------------------------------*/
       this.particlesMaterial = new THREE.PointsMaterial({
         color: 'red',
-        size: 0.05
+        size: 0.02
       })
+
+      /*---------------------------------
+      Particles Geometry
+      ---------------------------------*/
+      const sampler = new MeshSurfaceSampler(this.mesh).build()
+      const numParticles = 20000
+      this.particlesGeometry = new THREE.BufferGeometry()
+      const particlesPosition = new Float32Array(numParticles * 3)
+
+      for (let i = 0; i < numParticles; i++) {
+        const newPosition = new THREE.Vector3()
+        sampler.sample(newPosition)
+        particlesPosition.set([
+          newPosition.x,
+          newPosition.y,
+          newPosition.z
+        ], i * 3)
+      }
+
+      this.particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlesPosition, 3))
 
       /*---------------------------------
       Particles
       ---------------------------------*/
-      this.particles = new THREE.Points(this.geometry, this.particlesMaterial)
+      this.particles = new THREE.Points(this.particlesGeometry, this.particlesMaterial)
 
       /*---------------------------------
       Place on load
