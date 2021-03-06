e1a7ee111f701431878043b5bf88865e053bb953
Sven <sven.westerlaken@hotmail.nl>
2021-03-06 18:27:11 +0100

    add fog effect (fade out towards the back of the object)

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -12,6 +12,8 @@ void main() {
   vec3 color1 = vec3(10.0/255.0, 30.0/255.0, 100.0/255.0); // rgb(10, 30, 100)
   vec3 color2 = vec3(1.0, 1.0, 0.0);
 
+  float depth = vPosition.z * 0.5 + 0.5;
+
   color = mix(uColor1, uColor2, vPosition.z * 0.5 + 0.5);
-  gl_FragColor = vec4(color, 0.5);
+  gl_FragColor = vec4(color, depth * 0.3 + 0.2);
 }
