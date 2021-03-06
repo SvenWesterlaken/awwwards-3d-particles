6e8b65aa53a9522a5d4a5ffb2cc407267763e147
Sven <sven.westerlaken@hotmail.nl>
2021-03-06 17:04:45 +0100

    interpolate from middle

--- src/shaders/fragmentShader.glsl
+++ src/shaders/fragmentShader.glsl
@@ -6,8 +6,6 @@ void main() {
   vec3 color1 = vec3(1.0, 0.0, 0.0);
   vec3 color2 = vec3(1.0, 1.0, 0.0);
 
-
-
-  color = mix(color1, color2, vPosition.z);
+  color = mix(color1, color2, vPosition.z * 0.5 + 0.5);
   gl_FragColor = vec4(color, 1.0);
 }
