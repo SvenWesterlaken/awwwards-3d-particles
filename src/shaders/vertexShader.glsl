d742877dcdbc4386693c62704795e7cb9e09921a
Sven <sven.westerlaken@hotmail.nl>
2021-03-06 18:38:20 +0100

    use sin & cos

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -9,7 +9,9 @@ void main() {
   vPosition = position; // position is a variable automatically set by ThreeJS
 
   vec3 pos = position;
-  pos.x += uTime;
+  pos.x += sin(uTime) * 0.01;
+  pos.y += cos(uTime) * 0.01;
+  pos.z += cos(uTime) * 0.01;
 
   vec4 mvPosition = modelViewMatrix * vec4( pos, 1.0 );
   gl_Position = projectionMatrix * mvPosition;
